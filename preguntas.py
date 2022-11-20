c7df01b6427a5bcd0f630630a168284c9bc721dd
AnaGT <86501883+AnaGT@users.noreply.github.com>
2022-11-20 14:07:42 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -23,10 +23,10 @@ def pregunta_01():
     print(digits.target)
 
     # Imprima las dimensinoes de matriz de datos
-    print(digits.images.shape)
+    print(digits.data.shape)
 
     # Imprima las dimensiones del vector de salida
-    print(digits.data.shape)
+    print(digits.images.shape)
 
 
 def pregunta_02():
